9393822cc5df93b98678c134fb18b8abb0a2a00c
Ayush Rawal <31813648+Oguz-Khagan@users.noreply.github.com>
2017-12-05 00:29:57 +0530

    added database structure

--- converse.js
+++ converse.js
@@ -1,7 +1,21 @@
-angular.module("Converse")
+angular.module("Converse", ["firebase"])
     .controller("Conversations", Conversations)
 
-function Conversations() {
+function Conversations($firebaseArray, $firebaseAuth, $http) {
     var conv = this;
+    var auth = $firebaseAuth();
+    var database = $firebase.database();
+    conv.conversation = $firebaseArray(database.ref(conv.name));
 
-}
+    conv.send = function(t) {
+        conv.conversation.$add({ "message": t, "sender": conv.name }) //code to add message to firebase, timestamp to be added later
+        conv.text = "";
+    }
+
+    conv.login = function() {
+            auth.$signInWithPopup("google").then(function(firebaseUser) {
+                    console.log("Signed in as:", firebaseUser.user);
+                    console.log(firebaseUser.user.displayName)
+                    conv.name = firebaseUser.user.displayName;
+                }
+            }
