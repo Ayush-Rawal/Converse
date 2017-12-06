38c5462a81ab4997bd6fd4adea6172723349013e
Ayush Rawal <31813648+Oguz-Khagan@users.noreply.github.com>
2017-12-06 12:28:58 +0530

    added login bar, login page and OAuth to be added later

--- converse.js
+++ converse.js
@@ -6,16 +6,10 @@ function Conversations($firebaseArray, $firebaseAuth, $http) {
     var auth = $firebaseAuth();
     var database = $firebase.database();
     conv.conversation = $firebaseArray(database.ref(conv.name));
+    conv.name = firebaseUser.user.displayName;
 
     conv.send = function(t) {
         conv.conversation.$add({ "message": t, "sender": conv.name }) //code to add message to firebase, timestamp to be added later
         conv.text = "";
     }
-
-    conv.login = function() {
-            auth.$signInWithPopup("google").then(function(firebaseUser) {
-                    console.log("Signed in as:", firebaseUser.user);
-                    console.log(firebaseUser.user.displayName)
-                    conv.name = firebaseUser.user.displayName;
-                }
-            }
+}
--- signphone.js
+++ signphone.js
@@ -1,27 +1,29 @@
-var phoneNumber = getPhoneNumberFromUserInput();
-var appVerifier = window.recaptchaVerifier;
-firebase.auth().signInWithPhoneNumber(phoneNumber, appVerifier)
-    .then(function(confirmationResult) {
-        // SMS sent. Prompt user to type the code from the message, then sign the
-        // user in with confirmationResult.confirm(code).
-        window.confirmationResult = confirmationResult;
-    }).catch(function(error) {
-        // Error; SMS not sent
-        // ...
-    });
+//Code for signing up using phone number, to be added later
 
-var code = getCodeFromUserInput();
-confirmationResult.confirm(code).then(function(result) {
-    // User signed in successfully.
-    var user = result.user;
-    // ...
-}).catch(function(error) {
-    // User couldn't sign in (bad verification code?)
-    // ...
-});
+// var phoneNumber = getPhoneNumberFromUserInput();
+// var appVerifier = window.recaptchaVerifier;
+// firebase.auth().signInWithPhoneNumber(phoneNumber, appVerifier)
+//     .then(function(confirmationResult) {
+//         // SMS sent. Prompt user to type the code from the message, then sign the
+//         // user in with confirmationResult.confirm(code).
+//         window.confirmationResult = confirmationResult;
+//     }).catch(function(error) {
+//         // Error; SMS not sent
+//         // ...
+//     });
 
-firebase.auth().signOut().then(function() {
-    console.log('Signed out') // Sign-out successful.
-}).catch(function(error) {
-    console.log('Error in signing out using mobile') // An error happened.
-});
+// var code = getCodeFromUserInput();
+// confirmationResult.confirm(code).then(function(result) {
+//     // User signed in successfully.
+//     var user = result.user;
+//     // ...
+// }).catch(function(error) {
+//     // User couldn't sign in (bad verification code?)
+//     // ...
+// });
+
+// firebase.auth().signOut().then(function() {
+//     console.log('Signed out') // Sign-out successful.
+// }).catch(function(error) {
+//     console.log('Error in signing out using mobile') // An error happened.
+// });
